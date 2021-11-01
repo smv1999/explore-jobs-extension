7be9c2747767da5d8fc7a8b0b7a1bfdb107642ad
Vaidhyanathan S M <vaidhyanathan.sm@gmail.com>
2021-11-02 00:51:29 +0530

    Added main.html and main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+const jobs_url =
+  "https://weworkremotely.com/categories/remote-programming-jobs.rss";
+var e = document.createElement('div'); 
+fetch(jobs_url)
+  .then((response) => response.text())
+  .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then((data) => {
+    console.log(data);
+    const items = data.querySelectorAll("item");
+    let html = ``;
+    items.forEach((el) => {
+      html += `
+        <div>
+            <div class="list-header">
+                <h3>${el.querySelector("title").innerHTML}</h3>
+            </div>
+            <div class="list-content">
+                ${el.querySelector("description").textContent}
+            </div> 
+        </div>
+      `;
+    });
+    document.body.insertAdjacentHTML("beforeend", html);
+  });
